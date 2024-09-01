e1b5e0e59320d81393045779d3e2793a79ac4081
Paulo Henrique <paulo.phc666@gmail.com>
2024-09-01 04:03:22 -0300

    done

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+class Problems {
+    public void hasA(String str) {
+        int count = 0;
+
+        for (char letter: str.toCharArray()) {
+            if (letter == 'A' || letter == 'a') {
+                count++;
+            }
+        }
+
+        System.out.println(count == 0 ? "Não aparece." : "Aparece " + count + " vezes");
+    }
+
+    public boolean isFibonacci(int n) {
+        // f(n) = f(n - 1) + f(n - 2)
+        // 0 1 1 2 3 5 8 13 21...
+        if (n < 0) {
+            return false;
+        }
+
+        int a = 0, b = 1;
+        while (a < n) {
+            int temp = a;
+            a = b;
+            b = temp + a;
+        }
+
+        // returns true if input number is equal to
+        return a == n;
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Problems problems = new Problems();
+
+        String str = "AaaaaBBCC";
+        int number = 7;
+
+        problems.hasA(str);
+
+        System.out.println(problems.isFibonacci(number));
+
+    }
+}
